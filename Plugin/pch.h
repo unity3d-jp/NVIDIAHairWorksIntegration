3da5d1fcfaf37c657cfa37570bbc93c11068d95f
i-saint <saint.skr@gmail.com>
2015-09-02 21:11:18 +0900

    forgot to edit post-build event

--- Plugin/pch.h
+++ Plugin/pch.h
@@ -12,39 +12,19 @@
 
 using namespace DirectX;
 
-#ifdef _WIN32
-#define hwWindows
-#endif // _WIN32
 
 #define hwCLinkage extern "C"
-#ifdef _MSC_VER
 #define hwExport __declspec(dllexport)
-#else
-#define hwExport __attribute__((visibility("default")))
-#endif
 
 #ifdef hwDebug
-void hwDebugLogImpl(const char* fmt, ...);
-#define hwDebugLog(...) hwDebugLogImpl(__VA_ARGS__)
-#ifdef hwVerboseDebug
-#define hwDebugLogVerbose(...) hwDebugLogImpl(__VA_ARGS__)
+    void hwDebugLogImpl(const char* fmt, ...);
+    #define hwDebugLog(...) hwDebugLogImpl(__VA_ARGS__)
+    #ifdef hwVerboseDebug
+        #define hwDebugLogVerbose(...) hwDebugLogImpl(__VA_ARGS__)
+    #else
+        #define hwDebugLogVerbose(...)
+    #endif
 #else
-#define hwDebugLogVerbose(...)
+    #define hwDebugLog(...)
+    #define hwDebugLogVerbose(...)
 #endif
-#else
-#define hwDebugLog(...)
-#define hwDebugLogVerbose(...)
-#endif
-
-
-#ifdef hwWindows
-#include <windows.h>
-
-#ifndef hwNoAutoLink
-#endif // hwNoAutoLink
-
-#ifdef hwSupportD3D11
-#include <d3d11.h>
-#endif // hwSupportD3D11
-
-#endif // hwWindows
